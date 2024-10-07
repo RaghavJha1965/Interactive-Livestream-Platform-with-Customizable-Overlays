995ffb6015198096082445de555c74b72f3cdb52
Raghav Naresh Kumar jha <39232349+RaghavJha1965@users.noreply.github.com>
2024-10-06 23:00:53 -0400

    removing deployment changes

--- backend/StreamPlay.py
+++ backend/StreamPlay.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
-
+CORS(app)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client['overlay_db']
