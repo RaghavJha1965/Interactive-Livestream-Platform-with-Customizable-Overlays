14d2181d8d0c3f9f662af0c30a3dbf1c42d14256
Raghav Naresh Kumar jha <39232349+RaghavJha1965@users.noreply.github.com>
2024-10-06 22:44:06 -0400

    reversed the deployment

--- backend/StreamPlay.py
+++ backend/StreamPlay.py
@@ -1,11 +1,8 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
-CORS(app)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client['overlay_db']
