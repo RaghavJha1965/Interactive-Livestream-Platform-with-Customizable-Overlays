a9c62127226c9a69ea39480fba9625dd08153ce2
Raghav Naresh Kumar jha <39232349+RaghavJha1965@users.noreply.github.com>
2024-10-06 22:43:44 -0400

    reversed the deployment

--- livestream-overlay-app/src/api.jsx
+++ livestream-overlay-app/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://interactive-livestream-platform-with.onrender.com';
+const API_URL = 'http://localhost:5000';
 
 export const createOverlay = (overlayData) => axios.post(`${API_URL}/overlay`, overlayData);
 
